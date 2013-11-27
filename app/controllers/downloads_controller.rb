c2eec8d97acc0a6b5e3f6d24a4a1359a8aa3abd7
Anuj <anujjamwal@hotmail.com>
2013-11-27 11:00:50 +0530

    Fixing the log on downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,12 +1,13 @@
 class DownloadsController < ApplicationController
-  after_action :log_download
+  after_action :log_download, if: :success?
   
   def show
     @allocation = Allocation.find_by_hash_code(params[:id])
     render :not_found and return unless @allocation && @allocation.alive?(@current_user)
 
     @software = @allocation.software    
-    send_file @software.path
+    send_file @software.path, buffer_size: 4096, stream: true, type: 'application/octet-stream'
+    @success = true
   end
   
   private 
@@ -19,4 +20,8 @@ class DownloadsController < ApplicationController
       @download.save!
     end
   end  
+  
+  def success?
+    @success
+  end
 end
